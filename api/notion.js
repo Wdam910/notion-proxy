657feee83b4f9a399751f54bde298b30dd67e16a
SodaM <wdam0910@naver.com>
2025-12-24 23:50:40 +0900

    Add Notion API handler for database queries

---
+++ api/notion.js
@@ -0,0 +1,18 @@
+export default async function handler(req, res) {
+  const { databaseId } = req.query;
+
+  const response = await fetch(
+    `https://api.notion.com/v1/databases/${databaseId}/query`,
+    {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${process.env.NOTION_TOKEN}`,
+        'Notion-Version': '2022-06-28',
+        'Content-Type': 'application/json'
+      }
+    }
+  );
+
+  const data = await response.json();
+  res.status(200).json(data);
+}
